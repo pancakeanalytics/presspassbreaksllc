e3c7af942d5f286f16d1fb8e740f039f8ee38536
pancakeanalytics <datascience@pancakebreakfaststats.com>
2025-01-30 03:17:09 -0500

    Update grading_card.py

--- grading_card.py
+++ grading_card.py
@@ -11,7 +11,7 @@ import os
 DB_USER = os.getenv('DB_USER', 'pancakes_dev')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'Spiderman1001!')
 DB_NAME = os.getenv('DB_NAME', 'presspassbreaks')
-DB_CONNECTION_NAME = os.getenv('DB_CONNECTION_NAME', 'pancake-analytics-llc:us-central1:presspassbreaks')
+DB_CONNECTION_NAME = os.getenv('DB_CONNECTION_NAME', 'pancake-analytics-llc:us-central1:pancakes')
 
 # ✅ Define Cloud SQL Unix Socket Host (No IP Needed)
 DB_HOST = "34.171.57.16"
